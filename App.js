4ce9c44d40f99ee38b9f440b6015d19ecc95b600
AlexanderStewartCollege <alexander.stewart.developer@gmail.com>
2022-08-03 22:56:44 -0400

    .

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import { LogBox } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import AuthApp from './app/AuthApp';
@@ -7,6 +8,8 @@ import UIProvider from './app/context/UIContext';
 import UserProvider from './app/context/UserContext';
 
 export default function App() {
+
+  LogBox.ignoreAllLogs();
 
   return (
     <SafeAreaProvider>
